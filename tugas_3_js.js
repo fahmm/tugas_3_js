b23d48896a269ff2acf8012f4f459272f469113c
fahmi <pammimajid@gmail.com>
2020-03-11 20:11:52 +0800

    ok

--- tugas_3_js.js
+++ tugas_3_js.js
@@ -1,25 +1,4 @@
-//function yang mempunyai nilai balik 
-function nilai() {
-	var ok = "yes";
-	return ok;
-}
-var tampilkan = nilai();
-console.log(tampilkan);
 
-//function dengan parameter
-function data(a) {
-	console.log(a);
-	}
-data("tes");
-
-
-//function sebelum di return
-function nilaidata(a){
-	console.log(a);
-}
-nilaidata("siap");
-
-//function setelah di return
 function nilaidata(a){
 	console.log(a);
 	var kembali = "no";
